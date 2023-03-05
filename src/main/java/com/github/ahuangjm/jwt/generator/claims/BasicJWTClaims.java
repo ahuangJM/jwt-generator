5fd63fe44595a342856b5c37ceabc7a6b42e2c0f
AlanJ <jingming@ualberta.ca>
2023-03-05 01:20:54 -0700

    added JWTClaims and BasicJWTClaims

---
+++ src/main/java/com/github/ahuangjm/jwt/generator/claims/BasicJWTClaims.java
@@ -0,0 +1,4 @@
+package com.github.ahuangjm.jwt.generator.claims;
+
+public final class BasicJWTClaims extends JWTClaims {
+}
---
+++ src/main/java/com/github/ahuangjm/jwt/generator/claims/JWTClaims.java
@@ -0,0 +1,23 @@
+package com.github.ahuangjm.jwt.generator.claims;
+
+import lombok.Data;
+import sun.util.calendar.ZoneInfo;
+
+import java.util.Locale;
+
+@Data
+public abstract class JWTClaims implements RegisteredClaims, PublicClaims, PrivateClaims {
+    private String sub;
+    private String iss;
+    private String aud;
+    private Long exp;
+    private Long iat;
+
+    private String azp;
+    private String email;
+    private Boolean emailVerified;
+    private ZoneInfo zoneInfo;
+    private Locale locale;
+    private String sid;
+    private String csrf;
+}
