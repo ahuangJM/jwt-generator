bc0ab0b6644b9908b4514e0f074bb6855b02a7fd
AlanJ <jingming@ualberta.ca>
2023-03-05 00:30:04 -0700

    init

---
+++ src/test/java/com/github/ahuangjm/jwtgenerator/JwtGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ahuangjm.jwtgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
